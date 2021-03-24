5767b41a536a361f5097599b7c7163cc699fae61
chadhong <balato1992@gmail.com>
2021-03-24 17:02:46 +0800

    change project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('time-management app is running!');
+    expect(await page.getTitleText()).toEqual('time-management-tool app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'time-management'`, () => {
+  it(`should have as title 'time-management-tool'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('time-management');
+    expect(app.title).toEqual('time-management-tool');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('time-management app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('time-management-tool app is running!');
   });
 });
